d27a5bf52af8a42e10e12644609231833b27d206
Anastasiia <anastasiia.a.markova@gmail.com>
2024-03-13 15:24:13 +0300

    function for save images

--- booblik/booblik/rtsp_camera.py
+++ booblik/booblik/rtsp_camera.py
@@ -1,9 +1,11 @@
-
-from threading import Thread
-
 import cv2
 import numpy as np
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge  # Библиотека для конвертации между ROS Image и OpenCV Image
+
+from dataclasses import dataclass
+from threading import Thread
+import os
+from datetime import datetime
 
 import rclpy
 from rclpy.context import Context
@@ -12,8 +14,9 @@ from rclpy.parameter import Parameter
 from sensor_msgs.msg import Image
 
 
+@dataclass #декоратор
 class CameraConfig:
-    """Информация для подключения к камере через rtsp"""
+    """Информация для подключения к камере через RTSP"""
     login: str
     password: str
     ip: str
@@ -23,40 +26,50 @@ class CameraNode(Node):
     def __init__(self, node_name: str, config: CameraConfig) -> None:
         super().__init__(node_name)
         self.config = config
+        self.homedir = os.path.join(os.getcwd(), 'images')   # папка для сохранения изображений
+        
+        if not os.path.exists(self.homedir):
+            os.makedirs(self.homedir)
 
         # Создание издателя для публикации данных с камеры
         self.camera_ = self.create_publisher(
             Image,
             '/booblik/sensors/cameras/camera/image_raw',
             10)
-        self.cv_bridge = CvBridge()
+        self.cv_bridge = CvBridge()  # Объект для конвертации между OpenCV и ROS Image
+
 
     def start(self):
-        # Запуск потока для чтения данных с камеры
+        """Запуск потока для непрерывного чтения данных с камеры."""
         Thread(target=self._readLoop, daemon=False).start()
 
-    # Приватный метод для чтения данных с камеры
     def _readLoop(self):
-
+        """Чтение и публикация данных с камеры"""
         link = f"rtspsrc location=rtsp://\
             {self.config.login}:{self.config.password}@{self.config.ip}:{self.config.port}\
             /ISAPI/Streaming/Channels/102 latency=50 ! decodebin ! videoconvert ! appsink"
         
+        # Инициализация захвата видео с использованием GStreamer
         cap = cv2.VideoCapture(link, cv2.CAP_GSTREAMER)
 
         while True:
-            # Получение кадра
-            ok, img = cap.read()
-            # Если кадр был получен, то публикуем его в топик
+            ok, img = cap.read()  # Чтение кадра
             if ok:
-                self._push_image(img)
+                self._push_image(img)  # Публикация кадра, если чтение прошло успешно
+                self._save_image(img)   # Сохранение кадра
             else:
                 print('Ошибка чтения')
                 continue
 
     def _push_image(self, img: np.ndarray):
+        """Конвертация и публикация изображения в ROS топик."""
         ros2_img = self.cv_bridge.cv2_to_imgmsg(cvim=img, encoding='passthrough')
-        self.camera_.publish(ros2_img)
+        self.camera_.publish(ros2_img)  # Публикация изображения
+    
+    def _save_image(self, img: np.ndarray):
+        """Сохранение изображения с именем файла в формате временной метки."""
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        cv2.imwrite(os.path.join(self.homedir, f"{timestamp}.jpg"), img)
 
 def main(args=None):
     rclpy.init(args=args)
